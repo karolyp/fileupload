1b0040a4c568817c8ea6af9d3e34ec2ad221fe63
Karoly Pakozdi <Karoly.Pakozdi@logmein.com>
2020-02-28 22:22:32 +0100

    Adding Dockerfile, restructuring.

--- fileupload-backend/src/index.js
+++ fileupload-backend/src/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 const { APP_PORT } = require('./constants');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.status(200).send('<h1>Hello!</h1>');
+});
 
 app.listen(APP_PORT, () => {
   console.log(`App started on port ${APP_PORT}.`);
